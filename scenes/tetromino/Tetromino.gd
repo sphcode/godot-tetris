36bc1b5c6f0827026e509b52ed01b6d3de15ea0d
puhuasun <puhuasun@gmail.com>
2025-06-15 16:11:15 +0800

    Staying withing game bounds

--- scenes/tetromino/Tetromino.gd
+++ scenes/tetromino/Tetromino.gd
@@ -2,6 +2,12 @@ extends Node2D
 
 class_name Tetromino
 
+var bounds = {
+	"min_x": -216,
+	"max_x": 216,
+	"max_y": 457
+}
+
 var wall_kicks
 var tetromino_data
 var is_netx_piece
@@ -26,8 +32,6 @@ func _ready():
 	if !is_netx_piece:
 		position = tetromino_data.spawn_position
 		wall_kicks = Shared.wall_kicks_i if tetromino_data.tetromino_type == Shared.Tetromino.I else Shared.wall_kicks_jlostz
-		
-
 
 func _input(event):
 	if Input.is_action_just_pressed("left"):
@@ -40,6 +44,8 @@ func _input(event):
 		pass
 	elif Input.is_action_just_pressed("rotate_left"):
 		pass
+	elif Input.is_action_just_pressed("rotate_right"):
+		pass
 
 func move(direction: Vector2):
 	var new_position = calculate_global_position(direction, global_position)
@@ -49,8 +55,16 @@ func move(direction: Vector2):
 	return false
 
 func calculate_global_position(direction: Vector2, starting_global_position: Vector2):
+	if !is_within_game_bounds(direction, starting_global_position):
+		return null
 	return starting_global_position + direction * pieces[0].get_size().x
-	
+
+func is_within_game_bounds(direction: Vector2, starting_global_position: Vector2):
+	for piece in pieces:
+		var new_position = piece.position + starting_global_position + direction * piece.get_size()
+		if new_position.x < bounds.get("min_x") || new_position.x > bounds.get("max_x") || new_position.y >= bounds.get("max_y"):
+			return false
+	return true
 
 func _on_timer_timeout() -> void:
 	move(Vector2.DOWN)
